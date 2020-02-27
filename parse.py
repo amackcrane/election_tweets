663f9e372a481e1d1b3204cac226527d1225576f
Austen Mack-Crane <austen@austenmanesMBP7.fios-router.home>
2020-02-27 15:41:02 -0500

    attempt to filter out non-english tweets
    although it now looks like the topic model algorithm was more at fault...

--- parse.py
+++ parse.py
@@ -1,10 +1,16 @@
 
+import time
+start = time.time()
+
 import pandas as pd
 import pickle
+import sys
 
 import spacy
 import textacy
 from textacy.corpus import Corpus
+
+
 
 nlp = spacy.load("en_core_web_md")
 print("loaded")
@@ -21,10 +27,29 @@ tw_df = tw_df.loc[:, ["text", "retweeted", "created_at",
                       "followers_count", "favorite_count", "retweet_count"]]
 
 print(tw_df.head())
+print("Initial shape")
+print(tw_df.shape)
+
+# filter to english based on spacy vocabulary
+# hackishly, cuz tokenizing all properly would take too long
+
+# keep tweets with less than three unrecognizable tokens
+tw_df.loc[:,"eng"] = [
+    [nlp.vocab.strings[word] in nlp.vocab
+     for word in tw.split()].count(False) < 3
+    for tw in tw_df.text]
+
+test = tw_df.groupby('eng').agg({'text':'count'})
+print(test)
+
+tw_df = tw_df.iloc[tw_df.eng.values]
+print("Shape filtered to english-ish")
+print(tw_df.shape)
 
 # quick filter so this doesn't take forever
-tw_df = tw_df.sample(frac=.02)
-
+tw_df = tw_df.sample(frac=.03)
+print("Shape after sample")
+print(tw_df.shape)
 
 # persist data.frame format
 tw_df.to_pickle(".tweets.df")
@@ -37,14 +62,7 @@ tweets = Corpus(nlp, list(tw_df.text))
 tweets.save(".tweets.spacy")
 print("made .tweets.spacy")
 
-'''
-# get list of docs
-tweets = []
-for tw_text in tw_df.text:
-    tweets.append(nlp(tw_text))
-#tweets = list(map(nlp, tw_df.text))
 
-# persist
-with open(".tweets.spacy", "wb") as f:
-    pickle.dump((tweets, nlp.vocab), f)
-'''
+end = time.time()
+print("time (s):")
+print(end - start)
