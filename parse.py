5c9cf3defcb3dc18cd6b6f1a84e7878936e425ff
Austen Mack-Crane <austen@austenmanesMBP7.fios-router.home>
2020-03-12 12:07:36 -0400

    Added actual language detection to preprocessing

--- parse.py
+++ parse.py
@@ -9,10 +9,13 @@ import sys
 import spacy
 import textacy
 from textacy.corpus import Corpus
-
+from spacy_langdetect import LanguageDetector
 
 
 nlp = spacy.load("en_core_web_md")
+# add language detection
+nlp.add_pipe(LanguageDetector(), name='language_detector', last=True)
+
 print("loaded")
 
 
@@ -33,31 +36,41 @@ print(tw_df.shape)
 # filter to english based on spacy vocabulary
 # hackishly, cuz tokenizing all properly would take too long
 
+
+# this doesn't work very well...
 # keep tweets with less than three unrecognizable tokens
-tw_df.loc[:,"eng"] = [
-    [nlp.vocab.strings[word] in nlp.vocab
-     for word in tw.split()].count(False) < 3
-    for tw in tw_df.text]
-
-test = tw_df.groupby('eng').agg({'text':'count'})
-print(test)
-
-tw_df = tw_df.iloc[tw_df.eng.values]
-print("Shape filtered to english-ish")
-print(tw_df.shape)
+#tw_df.loc[:,"eng"] = [
+#    [nlp.vocab.strings[word] in nlp.vocab
+#     for word in tw.split()].count(False) < 3
+#    for tw in tw_df.text]
+#test = tw_df.groupby('eng').agg({'text':'count'})
+#print(test)
+#tw_df = tw_df.iloc[tw_df.eng.values]
+#print("Shape filtered to english-ish")
+#print(tw_df.shape)
 
 # quick filter so this doesn't take forever
-tw_df = tw_df.sample(frac=.03)
+tw_df = tw_df.sample(frac=.05)
 print("Shape after sample")
 print(tw_df.shape)
 
 # persist data.frame format
-tw_df.to_pickle(".tweets.df")
-print("made .tweets.df")
+#tw_df.to_pickle(".tweets.df")
+#print("made .tweets.df")
 # df.read_pickle
+########## Don't do that for now b/c we've lost alignment w/ the spacy corpus d/t lang filtering
 
 # draw into Spacy
 tweets = Corpus(nlp, list(tw_df.text))
+all_len = len(tweets)
+# filter english
+tweets.remove(lambda doc: doc._.language['language'] != 'en')
+#tweets = [doc for doc in tweets if doc._.language == 'en']
+en_len = len(tweets)
+pct = 100 - 100 * float(en_len) / all_len
+print("filtering to english dropped " + str(pct) + "% of tweets")
+# looks like 12%
+
 # persist
 tweets.save(".tweets.spacy")
 print("made .tweets.spacy")
