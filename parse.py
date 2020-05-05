64feb0781af479fa016b3534f2866f26d53b8b71
Austen Mack-Crane <4mackcrane@gmail.com>
2020-05-05 17:05:54 -0400

    simplified parsing; DataFrame usable again

--- parse.py
+++ parse.py
@@ -25,13 +25,18 @@ path = "election-day-tweets/election_day_tweets.csv"
 tw_df = pd.read_csv(path)
 
 
-tw_df["followers_count"] = tw_df["user.followers_count"]
-tw_df = tw_df.loc[:, ["text", "retweeted", "created_at",
-                      "followers_count", "favorite_count", "retweet_count"]]
-
 print(tw_df.head())
 print("Initial shape")
 print(tw_df.shape)
+
+# filter to english
+tw_df = tw_df.query("lang == 'en'")
+
+tw_df["followers_count"] = tw_df["user.followers_count"]
+tw_df["description"] = tw_df["user.description"]
+tw_df["handle"] = tw_df["user.screen_name"]
+tw_df = tw_df.loc[:, ["text", "retweeted", "created_at", "handle", "description",
+                      "followers_count", "favorite_count", "retweet_count"]]
 
 # filter to english based on spacy vocabulary
 # hackishly, cuz tokenizing all properly would take too long
@@ -50,30 +55,49 @@ print(tw_df.shape)
 #print(tw_df.shape)
 
 # quick filter so this doesn't take forever
-tw_df = tw_df.sample(frac=.05)
+tw_df = tw_df.sample(frac=.3)
 print("Shape after sample")
 print(tw_df.shape)
 
-# persist data.frame format
-#tw_df.to_pickle(".tweets.df")
-#print("made .tweets.df")
-# df.read_pickle
-########## Don't do that for now b/c we've lost alignment w/ the spacy corpus d/t lang filtering
 
 # draw into Spacy
-tweets = Corpus(nlp, list(tw_df.text))
-all_len = len(tweets)
+# this is the slow bit
+chunk_size = 10_000
+tweets = Corpus(nlp)
+for i in range((tw_df.shape[0] // chunk_size) + 1):
+    tweets.add(tw_df.text.iloc[chunk_size * i : chunk_size * (i+1)])
+    print("Parsed " + str(chunk_size * (i+1)) + " of " + str(tw_df.shape[0]))
+#tweets = Corpus(nlp, list(tw_df.text))
+
+""" Lolll the data.frame has a 'lang' column already
 # filter english
-tweets.remove(lambda doc: doc._.language['language'] != 'en')
+all_len = len(tweets)
+is_english = np.vectorize(lambda doc: doc._.language['lanugage'] == 'en')
+keep = is_english(tweets)
+to_remove = np.where(np.logical_not(keep))
+for i in to_remove:
+    del tweets[i]
+
+#tweets.remove(lambda doc: doc._.language['language'] != 'en')
 #tweets = [doc for doc in tweets if doc._.language == 'en']
+
+# filter english in data.frame too
+tw_df = tw_df.loc[keep]
+
 en_len = len(tweets)
 pct = 100 - 100 * float(en_len) / all_len
 print("filtering to english dropped " + str(pct) + "% of tweets")
 # looks like 12%
+"""
 
 # persist
 tweets.save(".tweets.spacy")
 print("made .tweets.spacy")
+
+# persist data.frame format
+tw_df.to_pickle(".tweets.df")
+print("made .tweets.df")
+# df.read_pickle
 
 
 end = time.time()
