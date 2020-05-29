8e9122ab344e163ebc15069f0e8a91c9b91875f6
Austen Mack-Crane <4mackcrane@gmail.com>
2020-05-29 17:51:32 -0400

    standardizing on MDS; winnowing graph methods; testing distance metrics

--- scripts/graph.py
+++ scripts/graph.py
@@ -10,9 +10,9 @@
 #'
 
 #'
-#' General-purpose language models learn embeddings for words that correspond to their syntactic niche and lexical meaning, since they're trained on broad corpora where words are used in a variety of contexts. This works well for tasks such as sentiment analysis and text classification where words can be largely understood in their common sense. But how do we approach a situation where, say, social actors marshal support for a political agenda using class-coded appeals to emotion? Pragmatics and social meanings -- how language is interpreted in particular social contexts; the ways it encodes identities and mediates relations -- subimpose beneath lexical meaning to sublimate delicate negotiations.
+#' General-purpose language models learn embeddings for words that correspond to their syntactic niche and lexical meaning, since they're trained on broad corpora where words are used in a variety of contexts. This works well for tasks such as sentiment analysis and text classification where words can be largely understood in their common sense. But how do we approach a situation where, say, social actors marshal support for a political agenda using class-coded appeals to emotion? Pragmatics and social meanings -- how language is interpreted in particular social contexts; the ways it encodes identities and mediates relations -- subimpose beneath lexical meaning to sublimate delicate negotiations^[see: plausible deniability].
 #'
-#' As representative of a general-purpose language model, we'll consider the word vectors included with Spacy's 'en-core-web-md'^[what are these], and we'll compare these with a few representations based on document-term matrices intended to capture the social situation of words. Our data comprise a corpus of tweets scraped based on the hashtag #Election2016 ^[over what time interval] and filtered to english.
+#' As representative of a general-purpose language model, we'll consider the word vectors included with Spacy's 'en-core-web-md'^[what are these], and we'll compare these with a few representations based on document-term matrices intended to capture the social situation of words. Our data comprise tweets scraped based on the hashtag #Election2016 ^[over what time interval?] and filtered to english.
 #'
 #'
 #' # Models for Pragmatics
@@ -34,7 +34,7 @@
 import spacy
 from sklearn.manifold import TSNE, MDS, Isomap, SpectralEmbedding
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
-from sklearn.metrics.pairwise import cosine_distances
+from sklearn.metrics.pairwise import cosine_distances, euclidean_distances, manhattan_distances
 from sklearn.decomposition import TruncatedSVD
 from sklearn.preprocessing import normalize
 from textacy.corpus import Corpus
@@ -87,7 +87,7 @@ unigrams = get_unigram_lists(tweets)
 #' * vector embeddings show some syntactic clustering, while pragmatic embeddings cluster syntactically distinct words together when they share a political valence
 #' * differences emerge for words with similar generic/topical meaning but particular partisan sense
 #'
-#' Let's pick out some words to pay particular attention to. We'll want some that have similar syntax but divergent meaning; some similar words with and without political overtones. Something we'll want to avoid is words that simply have a distinct *lexical* meaning in the context of elections.
+#' Let's pick out some words to pay particular attention to. We'll want some that have similar syntax but divergent meaning; some similar words with and without political overtones. But we should be careful of words that simply have a distinct *lexical* meaning in the context of elections.
 #'
                                         #+ peek_words, results='show'
                                         
@@ -123,18 +123,18 @@ print(ref_words)
                                         #+ plot_vec, results='show'
 
 top_vec = get_spacy_vectors(ref_words)
-top_dist = cosine_distances(top_vec)
+top_cos = cosine_distances(top_vec)
 
 tsne = TSNE(perplexity=5, metric='precomputed')
-mds = MDS(dissimilarity='precomputed', n_init=20)
+mds = MDS(dissimilarity='precomputed', random_state=100)
 umap = UMAP(metric='precomputed')
 svd = TruncatedSVD(algorithm='arpack')
 
 _, ax = plt.subplots(2,2, figsize=(12,12))
 
-plot_embed(top_dist, tsne, ax[0][0], "t-SNE", filter=False)
-plot_embed(top_dist, mds, ax[0][1], "MDS", filter=False)
-plot_embed(top_dist, umap, ax[1][0], "UMAP", filter=False)
+plot_embed(top_cos, tsne, ax[0][0], "t-SNE", filter=False)
+plot_embed(top_cos, mds, ax[0][1], "MDS", filter=False)
+plot_embed(top_cos, umap, ax[1][0], "UMAP", filter=False)
 plot_embed(top_vec, svd, ax[1][1], "SVD (on plain data)", filter=False)
 
 plt.show()
@@ -148,6 +148,26 @@ plt.show()
 #' MDS shows less distinct clustering; it has a similar configuration of 'civic' terms and family/news/pray/change; it intermingles demographic and epithetic terms more than the others; and it places 'maga' and 'imwithher' off to the side.
 #'
 #'
+
+#'
+#' ### Euclidean, Cosine, and Manhattan distance
+#'
+#' (using MDS)
+#' 
+
+top_euclid = euclidean_distances(top_vec)
+top_manh = manhattan_distances(top_vec)
+top_manh_norm = normalized_manhattan(top_vec)
+
+_, ax = plt.subplots(2,  2, [12,12])
+
+plot_embed(top_euclid, mds, ax[0][0], "Euclidean", filter=False)
+plot_embed(top_cos, mds, ax[0][1], "Cosine", filter=False)
+plot_embed(top_manh, mds, ax[1][0], "Manhattan", filter=False)
+plot_embed(top_manh_norm, mds, ax[1][1], "Normalized Manhattan", filter=False)
+
+plt.show()
+
 #'
 #' 
 #' # Handle-Word Matrices
@@ -208,58 +228,45 @@ plt.show()
 
 cv = TfidfVectorizer(analyzer = lambda x: x)
 
-fit_cv(min_df=10)
+fit_cv(min_df=30)  # was 10
 ref_inds = reference_indices()
 
-umap.n_neighbors=3
 
 word_user = get_matrix(handles).transpose()
 word_user_rec = get_matrix(rec_handles).transpose()
-word_dist = cosine_distances(word_user)
-word_dist_rec = cosine_distances(word_user_rec)
-
+word_cos = cosine_distances(word_user)
+word_cos_rec = cosine_distances(word_user_rec)
+word_manh = normalized_manhattan(word_user)
+word_manh_rec = normalized_manhattan(word_user_rec)
 
 #'
-#' ### UMAP on cosine distance
+#' ### MDS on cosine distance
 #' 
                                         #+ twomode_plot, results='show'
 _, ax = plt.subplots(1,2, figsize=(12,7))
 
-plot_embed(word_dist, umap, ax[0], "All Handles")
-plot_embed(word_dist_rec, umap, ax[1], "Recurring Handles")
+plot_embed(word_cos, mds, ax[0], "All Handles")
+plot_embed(word_cos_rec, mds, ax[1], "Recurring Handles")
 
 plt.show()
 
 #'
-#' ### SVD on term-user matrix
+#' ### Metric Comparison
+#'
+                                        #+ twomode_dist, results='show'
+
+_, ax = plt.subplots(1,2, figsize=[12,7])
+
+plot_embed(word_cos, mds, ax[0], "Cosine on All Handles")
+plot_embed(word_manh, mds, ax[1], "Normalized Manhattan on All Handles")
+
+plt.show()
+
+
+#'
+#' (Note the difference between looking at socio-semantic relationships in a population vs. a 'community' such as might be found via socio-semantic community detection. We're looking at word relationships with respect to variance in usage among people; we could also restrict ourselves to a community of users with shared meaning systems, and look at patterns of word co-occurrence...) 
+#'
 #' 
-                                        #+ twomode_svd, results='show'
-
-_, ax = plt.subplots(1, 2, figsize=[12,7])
-
-plot_embed(word_user, svd, ax[0], "All Handles (SVD)")
-plot_embed(word_user_rec, svd, ax[1], "Recurring Handles (SVD)")
-
-plt.show()
-
-
-#'
-#' ### UMAP on euclidean distance
-#'
-                                        #+ twomode_plot2, results='show', eval=FALSE
-
-_, ax = plt.subplots(1,2, figsize=(12,7))
-
-umap.metric='euclidean'
-# gets an error for low n_neighbors?
-umap.n_neighbors=10
-
-plot_embed(word_user, umap, ax[0], "All Handles")
-plot_embed(word_user_rec, umap, ax[1], "Recurring Handles")
-
-plt.show()
-
-#'
 #' # Graph Methods
 #'
 #' In addition to looking at the handle-word matrix as such, we can see this matrix of co-occurrences between words and users as implying a network of relations among words [@Breiger1974], following the "distributional" assumption that co-occurring words are similar^[Of course, following our interest in different scales of social variance, we must elaborate at some point what kind of similarity we infer from co-occurrence.].
@@ -282,7 +289,6 @@ word_word_dist = word_word.copy()
 convert_to_distances(word_word_dist) # "distance" as inverse of path count
 
 #'
-#' Isomap is a manifold-based dimensionality-reduction algorithm sort of close to what we want to do: it does MDS on shortest paths in a weighted graph of euclidean distances to nearest neighbors. First, we check how different that is to a simpler MDS on shortest paths from dissimilarities. The difference should be that Isomap draws the graph based on just a few nearest neighbors, whereas the DIY way potentially draws all edges.
 #'
 #' I think both of these methods are sort of confused, because taking distance as 'inverse of co-occurrence rate' is unlikely to yield a coherent set of distances, nor a situation where shortest path length is meaningful. We might inspect
 #'
@@ -293,67 +299,27 @@ convert_to_distances(word_word_dist) # "distance" as inverse of path count
 #'
 
 
-                                        #+ test_nx, results='show'
+                                        #+ setup_nx, results='show'
 
 # SLOW
 G = nx.convert_matrix.from_numpy_array(word_word_bin.todense())
 layout_dict = nx.drawing.layout.spring_layout(G)
 
-plot_graph(layout_dict, plt, "Spring Layout of Binarized Graph")
 
-plt.show()
-
-                                        #+ onemode_plot, results='show', eval=FALSE
+                                        #+ onemode_plot, results='show'
 # Isomap vs. DIY
-_, ax = plt.subplots(1, 2, figsize=[12,7])
-
-# isomap
-iso = Isomap(metric="precomputed")
-plot_embed(word_word_dist, iso, ax[0], "Isomap")
-
-# DIY
-paths = shortest_path(word_word_dist)
-plot_embed(paths, mds, ax[1], "MDS on Shortest Paths")
-
-plt.show()
-
-
-                                        #+ iso_spec, results='show', eval=FALSE
-# Isomap vs Spectral
-
-_, ax = plt.subplots(1, 2, figsize=[12,7])
+_, ax = plt.subplots(2, 2, figsize=[12,12])
 
 # MDS
-plot_embed(word_word_dist.toarray(), mds, ax[0], "MDS on pseudo-distances")
+paths = shortest_path(word_word_dist)
+plot_embed(paths, mds, ax[0][0], "MDS on Shortest Paths")
+
+# Graph Drawing
+plot_graph(layout_dict, ax[0][1], "Spring Layout of Binarized Graph")
 
 # spectral embed
 spec = SpectralEmbedding(affinity="precomputed")
-plot_embed(word_word, spec, ax[1], "Spectral Embedding (logged)", log=True)
-# warning: graph not fully connected, SE may not work great
-# - this was for rec_handles. all good w/ all handles
-
-plt.show()
-
-
-                                        #+ iso_umap, results='show', eval=FALSE
-
-_, ax = plt.subplots(1, 2, figsize=[12,7])
-
-# Isomap on tfidf
-#  should up-weight co-occurrences of rare words...
-cv = TfidfVectorizer(analyzer = lambda x: x)
-fit_cv(min_df=30, _handles=which_handles)
-user_tfidf = get_matrix(which_handles)
-word_tfidf = get_one_mode(user_tfidf)
-convert_to_distances(word_tfidf)
-
-plot_embed(word_tfidf, iso, ax[0], "Isomap - tf-idf")
-
-
-# UMAP on shortest paths
-umap.n_neighbors=15
-umap.metric="precomputed"
-plot_embed(paths, umap, ax[1], "UMAP on psuedo path lengths from tf-idf")
+plot_embed(word_word, spec, ax[1][0], "Spectral Embedding")
 
 plt.show()
 
