5e9095ed821bcb67f30a5c0e21b11a561d5c7500
Austen Mack-Crane <4mackcrane@gmail.com>
2020-06-16 21:31:34 -0400

    tidy

--- scripts/parse.py
+++ scripts/parse.py
@@ -9,12 +9,9 @@ import sys
 import spacy
 import textacy
 from textacy.corpus import Corpus
-from spacy_langdetect import LanguageDetector
 
 
 nlp = spacy.load("en_core_web_md")
-# add language detection
-nlp.add_pipe(LanguageDetector(), name='language_detector', last=True)
 
 print("loaded")
 
@@ -38,21 +35,6 @@ tw_df["handle"] = tw_df["user.screen_name"]
 tw_df = tw_df.loc[:, ["text", "retweeted", "created_at", "handle", "description",
                       "followers_count", "favorite_count", "retweet_count"]]
 
-# filter to english based on spacy vocabulary
-# hackishly, cuz tokenizing all properly would take too long
-
-
-# this doesn't work very well...
-# keep tweets with less than three unrecognizable tokens
-#tw_df.loc[:,"eng"] = [
-#    [nlp.vocab.strings[word] in nlp.vocab
-#     for word in tw.split()].count(False) < 3
-#    for tw in tw_df.text]
-#test = tw_df.groupby('eng').agg({'text':'count'})
-#print(test)
-#tw_df = tw_df.iloc[tw_df.eng.values]
-#print("Shape filtered to english-ish")
-#print(tw_df.shape)
 
 # quick filter so this doesn't take forever
 tw_df = tw_df.sample(frac=.3)
@@ -69,33 +51,13 @@ for i in range((tw_df.shape[0] // chunk_size) + 1):
     print("Parsed " + str(chunk_size * (i+1)) + " of " + str(tw_df.shape[0]))
 #tweets = Corpus(nlp, list(tw_df.text))
 
-""" Lolll the data.frame has a 'lang' column already
-# filter english
-all_len = len(tweets)
-is_english = np.vectorize(lambda doc: doc._.language['lanugage'] == 'en')
-keep = is_english(tweets)
-to_remove = np.where(np.logical_not(keep))
-for i in to_remove:
-    del tweets[i]
-
-#tweets.remove(lambda doc: doc._.language['language'] != 'en')
-#tweets = [doc for doc in tweets if doc._.language == 'en']
-
-# filter english in data.frame too
-tw_df = tw_df.loc[keep]
-
-en_len = len(tweets)
-pct = 100 - 100 * float(en_len) / all_len
-print("filtering to english dropped " + str(pct) + "% of tweets")
-# looks like 12%
-"""
 
 # persist
-tweets.save(".tweets.spacy")
+tweets.save(".data/tweets.spacy")
 print("made .tweets.spacy")
 
 # persist data.frame format
-tw_df.to_pickle(".tweets.df")
+tw_df.to_pickle(".data/tweets.df")
 print("made .tweets.df")
 # df.read_pickle
 
